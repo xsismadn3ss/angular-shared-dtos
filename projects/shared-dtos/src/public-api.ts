fffa7597189be11209a22caa44c50817b0aa75d3
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-03 22:26:24 -0600

    Agregar exportaciones públicas para DTOs compartidos

---
+++ projects/shared-dtos/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of shared-dtos
+ */
+// common
+export * from'./lib/common/message.dto'
+
+// users/auth
+export * from './lib/users/auth/user.dto'
+export * from './lib/users/auth/login.dto'
+export * from './lib/users/auth/auth.service'
