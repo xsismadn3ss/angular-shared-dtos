6db52404eb28c5e14fed0cecaa6b226ced0d9681
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-05 01:32:18 -0600

    Actualizar DTO de inicio de sesión y hacer que herede de BaseDto, también se ha agregado documentación a los DTO

--- projects/shared-dtos/src/lib/users/auth/login.dto.ts
+++ projects/shared-dtos/src/lib/users/auth/login.dto.ts
@@ -1,19 +1,49 @@
 import {IsNotEmpty, IsString} from "class-validator";
 import {MessageDto} from '../../common/message.dto';
+import {BaseDto} from '../../common/base.dto';
 
-export class LoginDto {
-  domain: string = "colibrihub.com"
+/**
+ * ### ``LoginDto``
+ * DTO para iniciar sesión
+ */
+export class LoginDto extends BaseDto {
+  /**
+   * ### dominio
+   * Se utiliza para especificar el dominio que se usara para crear
+   * la cookie que será usada como token de acceso
+   */
+  @IsString()
+  domain: string = ""
 
+  /**
+   * ### username
+   * Nombre de usuario
+   */
   @IsString()
   @IsNotEmpty({message: 'Username is required'})
   username!: string;
 
+  /**
+   * ### password
+   * Contraseña
+   */
   @IsString()
   @IsNotEmpty({message: 'Password is required'})
   password!: string;
 }
 
+/**
+ * ### ``LoginResponseDto``
+ * Respuesta obtenida al haber iniciado sesión correctamente
+ */
 export class LoginResponseDto extends MessageDto{
+  /**
+   * token de acceso obtenido al iniciar sesión
+   */
   token!: string;
+  /**
+   * nombre de usuario brindado por el servidor para tener contexto
+   * de qué usuario ha iniciado sesión
+   */
   username!: string;
 }
