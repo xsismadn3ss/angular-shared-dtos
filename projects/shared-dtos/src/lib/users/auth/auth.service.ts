27ace2b2e72f984f3caa53ec0d32eddfd6d471f3
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-05 01:32:47 -0600

    Añadir documentación al servicio de autenticación

--- projects/shared-dtos/src/lib/users/auth/auth.service.ts
+++ projects/shared-dtos/src/lib/users/auth/auth.service.ts
@@ -1,52 +1,49 @@
-import {Inject, inject, Injectable, InjectionToken, isDevMode, Optional} from '@angular/core';
+import {inject, Injectable, isDevMode} from '@angular/core';
 import {LoginResponseDto, LoginDto} from './login.dto';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {MessageDto} from '../../common/message.dto';
 
 /**
- * ``AUTH_API_URL``
- * <br>
- * Token de inyección que representa la URL base de la API de autenticación.
- * Este token se utiliza para configurar y proporcionar el punto final donde
- * se realizarán las operaciones de autenticación, como el inicio de sesión
- * o la validación del token.
- * ---
- * ### Implementación
- * ```typescript
- * import {AUTH_API_URL} from 'shared-dtos';
- *
- * @NgModule({
- *    declarations: [AppComponent],
- *    imports: [BrowserModule, HttpClientModule],
- *    providers:[
- *      // la url de la API debe ser manejada como variable de entorno, no debe estar pública
- *      { provide: AUTH_API_URL, useValue: 'https://api.example.com'} // <- API de autenticación
- *    ],
- *    bootstrap: [AppComponent]
- * })
- * export class AppModule
- * ```
- */
-export const AUTH_API_URL = new InjectionToken('AUTH_API_URL')
-
-/**
- * ## ``Auth Service``
+ * # ``Auth Service``
  * Servicio de autenticación, utiliza este servicio público para
  * autenticar a los usuarios.
  * ---
- * ## Ejemplo de uso
- * ```typescript
- * import {AuthService} from 'shared-dtos'
+ * ## Configurar URL para el servicio
+ * En el archivo ``app.config.ts`` en el proyecto de angular, agrega la siguiente configuración
+ * ```ts
+ * export const appConfig: ApplicationConfig = {
+ *   providers: [
+ *     provideZoneChangeDetection({ eventCoalescing: true }),
+ *     provideRouter(routes),
+ *     provideHttpClient(),
+ *     // configurar URL para el servicio AuthService
+ *     {
+ *       provide: AuthService,
+ *       useFactory: () => {
+ *         // esta URL debe ser manejada como variable de entorno, no debe estar
+ *         // pública en el repositorio
+ *         const baseUrl = '<AUTH_SERVICE_URL>';
+ *         return new AuthService(baseUrl);
+ *       }
+ *     }
+ *   ]
+ * };
+ * ```
+ *---
+ * ## Inyectar dependencia
+ * ```ts
+ * import {AuthService} from '@xsismadn3ss/shared-dtos'
  *
  * @Component({
  *    selector: 'app-login',
+ *    template: ´...´
  * })
  * export class LoginComponent {
  *
- *  // inyectar dependencia
  *  constructor(private readonly auth: AuthService){}
  *
+ * //...
  * }
  * ```
  * Asegurate de inyectar el token para la URL del servicio, revisa la
@@ -58,8 +55,14 @@ export const AUTH_API_URL = new InjectionToken('AUTH_API_URL')
 export class AuthService {
   private readonly  http = inject(HttpClient)
 
-  constructor(@Optional() @Inject(AUTH_API_URL) private readonly baseUrl?: string | null) {}
+  constructor(private readonly baseUrl?: string | null) {}
 
+  /**
+   * ### Login
+   * Utiliza esta función para iniciar sesión
+   * @param credentials ``LoginDto`` datos de inicio de sesión
+   * @return ``LoginResponseDto`` respuesta de inicio de sesión
+   */
   login(credentials: LoginDto): Observable<LoginResponseDto>{
     const res = this.http.post<LoginResponseDto>(this.baseUrl + '/authentication/login', credentials);
 
@@ -72,6 +75,11 @@ export class AuthService {
     return res;
   }
 
+  /**
+   * ### Logout
+   * Cerrar sesión eliminando el token de acceso del usuario
+   * del navegador
+   */
   logout(): Observable<MessageDto>{
     if(isDevMode()){
       document.cookie = `token=; max-age=0; path=/;`
