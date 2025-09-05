544d41080e1dbf4f78c7bb53d504c977f5dabb4c
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-05 01:30:25 -0600

    Añadir documentación al DTO de mensajes

--- projects/shared-dtos/src/lib/common/message.dto.ts
+++ projects/shared-dtos/src/lib/common/message.dto.ts
@@ -1,3 +1,13 @@
-export class MessageDto {
+/**
+ * ### ``MessageDto``
+ * DTO para las respuestas que el parámetro message en el
+ * cuerpo de la petición
+ */
+export class MessageDto{
+  /**
+   * ### message
+   * Texto descriptivo que contiene información
+   * acerca de la respuesta obtenida
+   */
   message!: string;
 }
