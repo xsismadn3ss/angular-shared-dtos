64e70204c21643e6b0752e3c1e0536bda4e068ba
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-06 01:26:49 -0600

    ♻️ refactorizar: actualizar guard y mejorar lógica de negocio

--- projects/shared-dtos/src/lib/users/validation/validation.guard.ts
+++ projects/shared-dtos/src/lib/users/validation/validation.guard.ts
@@ -6,29 +6,19 @@ import {catchError, map, of} from 'rxjs';
 
 export const validationGuard: CanActivateFn = (_route, state)=>{
   const validation = inject(ValidationService);
-  const router = inject(Router);
   const loginUrl = inject(AUTH_LOGIN_URL, {optional: false});
 
-  return validation.validate().pipe(
-    map(()=> true),
-    catchError((_err)=>{
-      const returnUrl = encodeURIComponent(state.url)
-
-      if (loginUrl) {
-        // Si la URL de login es absoluta (otro dominio), usa redirección de navegador
-        const isAbsolute = /^https?:\/\//i.test(loginUrl);
-        const sep = loginUrl.includes('?') ? '&' : '?';
-        const target = `${loginUrl}${sep}returnUrl=${returnUrl}`;
-
-        if (isAbsolute) {
-          window.location.assign(target);
-        } else {
-          // Login interno con ruta relativa
-          router.navigate([loginUrl], { queryParams: { returnUrl: state.url } });
-        }
-      } else {
-        throw Error("No se ha configurado la URL de inicio de sesión")
-      }
+  return  validation.validate().pipe(
+    map((u)=>{
+      const data = JSON.stringify(u)
+      document.cookie = `user=${data}`
+      console.log('Sesión valida con exito')
+      return true
+    }),
+    catchError((_err) => {
+      const returnUrl = decodeURIComponent(state.url)
+      window.location.href = `${loginUrl}?redirect=${returnUrl}`
+      console.error('No se pudo validar la sesión')
       return of(false)
     })
   )
